d1360d4502762ca936489f60de98d9638d111193
Atreya <84874851+atreyasinha@users.noreply.github.com>
2021-11-13 16:57:28 -0800

    correct shareholder value and precision

--- sscapital/views.py
+++ sscapital/views.py
@@ -63,22 +63,22 @@ class Admin(View):
 
             # Fund
             prometheus_returns = round((((prometheus_value - prometheus_capital) / prometheus_value) * 100), 2)
-            prometheus_data = Prometheus(id=1, capital=prometheus_capital, value=prometheus_value, returns=prometheus_returns)
+            prometheus_data = Prometheus(id=1, capital=round(prometheus_capital), value=round(prometheus_value), returns=prometheus_returns)
             prometheus_data.save()
 
             # Investors
             financial_data = Financials.objects.get(investor=investor.id)
-            financial_data.market_value = financial_value
-            financial_data.capital_invested = financial_capital
+            financial_data.market_value = round(financial_value)
+            financial_data.capital_invested = round(financial_capital)
             financial_data.returns = round((((financial_value - financial_capital) / financial_capital) * 100), 2)
-            financial_data.shareholder = round((prometheus_value / financial_value), 2)
+            financial_data.shareholder = round((financial_value / prometheus_value) * 100, 2)
             financial_data.save()
 
 
         if form_update.is_valid():
             value = form_update.cleaned_data['value']
             data = Prometheus.objects.get(id=1)
-            data.value = value
+            data.value = round(value)
             difference = float(value) - data.capital
             data.returns = round((difference / data.capital) * 100, 2)
             data.save()
@@ -87,7 +87,7 @@ class Admin(View):
                 if i.capital_invested == 0:
                     continue
                 financial_value = round(i.market_value + (i.shareholder * difference) / 100, 2)
-                i.market_value = financial_value
+                i.market_value = round(financial_value)
                 i.returns = round(((financial_value - i.capital_invested) / i.capital_invested) * 100, 2)
                 i.shareholder = round(data.value / financial_value, 2)
                 i.save()
